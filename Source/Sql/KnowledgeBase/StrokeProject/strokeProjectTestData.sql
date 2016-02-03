2b6ed9024ae41db3be96622921a5097867691f79
mduby <mduby@broadinstitute.org>
2016-02-03 17:36:46 -0500

    success adding dummy stroke tables for test

--- Source/Sql/KnowledgeBase/StrokeProject/strokeProjectTestData.sql
+++ Source/Sql/KnowledgeBase/StrokeProject/strokeProjectTestData.sql
@@ -27,13 +27,22 @@ delete from ID_PH where lower(ID) like '%39k%';
 insert into ID_PH (ID, PH, TBL) values('ExSeq_39k_mdv5', 'STRK', 'ExSeq_39k_dv5__STRK');
 insert into ID_PH (ID, PH, TBL) values('ExSeq_39k_eu_mdv5', 'STRK', 'ExSeq_39k_eu_dv5__STRK');
 
--- add dataset poperties for the stroke mdv
+-- add dataset properties for the stroke mdv
 delete from PROP_ID where lower(ID) like '%39k%';
 insert into PROP_ID (PROP, ID) select PROP, 'ExSeq_39k_mdv5' from PROP_ID where ID = 'ExSeq_17k_mdv2';
 insert into PROP_ID (PROP, ID) select PROP, 'ExSeq_39k_eu_mdv5' from PROP_ID where ID = 'ExSeq_17k_eu_mdv2';
 
--- add in tables to dataset
+-- add phenotype properties for the stroke mdv
+delete from PROP_ID_PH where lower(ID) like '%39k%';
+insert into PROP_ID_PH (PROP, ID, PH, TRUST_COL) select PROP, 'ExSeq_39k_mdv5', 'STRK', TRUST_COL from PROP_ID_PH where ID = 'ExSeq_17k_mdv2';
+insert into PROP_ID_PH (PROP, ID, PH, TRUST_COL) select PROP, 'ExSeq_39k_eu_mdv5', 'STRK', TRUST_COL from PROP_ID_PH where ID = 'ExSeq_17k_eu_mdv2';
 
+-- add in tables to dataset
+delete from DATASET where lower(ID) like '%39k%';
+insert into DATASET (ID, EXP, SG, VER, PARENT, ANCESTRY, TECH, TBL, SORT, CASES, CONTROLS, SUBJECTS) 
+select 'ExSeq_39k_mdv5', 'ExSeq_39k', '39k', 'mdv5', 'Root', ANCESTRY, TECH, 'ExSeq_39k_dv5', 98, CASES, CONTROLS, SUBJECTS from DATASET where ID = 'ExSeq_17k_mdv2';
+insert into DATASET (ID, EXP, SG, VER, PARENT, ANCESTRY, TECH, TBL, SORT, CASES, CONTROLS, SUBJECTS) 
+select 'ExSeq_39k_eu_mdv5', 'ExSeq_39k', '39k_eu', 'mdv5', '39k', ANCESTRY, TECH, 'ExSeq_39k_eu_dv5', 99, CASES, CONTROLS, SUBJECTS from DATASET where ID = 'ExSeq_17k_eu_mdv2';
 
 
 
