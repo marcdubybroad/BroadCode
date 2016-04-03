13f6bd7fdc189ac47b729ad77ac07045c5950a76
mduby <mduby@broadinstitute.org>
2016-04-03 13:23:28 -0400

    adding burden json sample creator extended

--- Source/Sql/KnowledgeBase/201604BurdenExpand/burdenGetDataSamplesJsonCreator.sql
+++ Source/Sql/KnowledgeBase/201604BurdenExpand/burdenGetDataSamplesJsonCreator.sql
@@ -3,6 +3,11 @@
 
 select
 concat('[{ "ID": { "SAMPLES_13k_mdv2": "', ifnull(ID, 'NULL'), '"}},') as id,
+concat('{ "ANCESTRY": { "SAMPLES_13k_mdv2": "', ifnull(ANCESTRY, 'NULL'), '"}},') as ancestry,
+concat('{ "T2D": { "SAMPLES_13k_mdv2": ', ifnull(T2D, 'NULL'), '}},') as t2d,
+concat('{ "AGE": { "SAMPLES_13k_mdv2": ', ifnull(AGE, 'NULL'), '}},') as age,
+concat('{ "FAST_GLU": { "SAMPLES_13k_mdv2": ', ifnull(FAST_GLU, 'NULL'), '}},') as fast_glu,
+concat('{ "FAST_INS": { "SAMPLES_13k_mdv2": ', ifnull(FAST_INS, 'NULL'), '}},') as fast_ins,
 concat('{ "BMI": { "SAMPLES_13k_mdv2": ', ifnull(BMI, 'NULL'), '}},') as bmi,
 concat('', '{ "WEIGHT": { "SAMPLES_13k_mdv2": ', ifnull(WEIGHT, 'NULL'), '}},') as weight,
 concat('', '{ "HDL": { "SAMPLES_13k_mdv2": ', ifnull(HDL, 'NULL'), '}},') as hdl,
@@ -10,14 +15,13 @@ concat('', '{ "LDL": { "SAMPLES_13k_mdv2": ', ifnull(LDL, 'NULL'), '}}],') as ld
 from SAMPLE_13k 
 limit 10;
 
-where BMI != null and HDL != null and LDL != null
-
-'{ "BMI": { "SAMPLES_13k_mdv2": ' | BMI | '}},'
 
 
 
-{
-    "MAF": {
-        "SAMPLES_13k_mdv2": 0.127025738798856
-    }
-},
+--where BMI != null and HDL != null and LDL != null
+--'{ "BMI": { "SAMPLES_13k_mdv2": ' | BMI | '}},'
+--{
+--    "MAF": {
+--        "SAMPLES_13k_mdv2": 0.127025738798856
+--    }
+--},
