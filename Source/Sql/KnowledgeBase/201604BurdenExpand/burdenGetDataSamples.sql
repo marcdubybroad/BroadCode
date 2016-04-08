67a91108cca2c4fcb808b6209b9cc380392a3e99
mduby <mduby@broadinstitute.org>
2016-04-08 13:51:26 -0400

    added PH link and common tables

--- Source/Sql/KnowledgeBase/201604BurdenExpand/burdenGetDataSamples.sql
+++ Source/Sql/KnowledgeBase/201604BurdenExpand/burdenGetDataSamples.sql
@@ -1,4 +1,13 @@
 
+-- common table
+-- drop table if exists
+drop table if exists SAMPLES_common_dv1;
+create table SAMPLES_common_dv1 (ID text null);
+
+-- add in the rows from the samples tables
+insert into SAMPLES_common_dv1 (select ID from SAMPLE_13k s13 where not exists (select * from SAMPLES_common_dv1 where ID = s13.ID));
+insert into SAMPLES_common_dv1 (select ID from SAMPLE_26k s13 where not exists (select * from SAMPLES_common_dv1 where ID = s13.ID));
+
 -- DATASET
 -- drop table if exists
 drop table if exists SAMPLES_DATASET;
@@ -7,9 +16,10 @@ drop table if exists SAMPLES_DATASET;
 create table SAMPLES_DATASET select * from DATASET;
 
 -- remove all old rows
-delete from SAMPLES_DATASET;
+delete from SAMPLES_DATASET;x
 
 -- add in the samples rows
+insert into SAMPLES_DATASET (ID, EXP, SG, VER, PARENT, TBL, SORT) values('samples_common_dv1', 'null', 'NULL', 'mdv1', 'Root', 'SAMPLES_common_dv1', 60);
 insert into SAMPLES_DATASET (ID, EXP, VER, PARENT, TBL, SORT) values('samples_13k_mdv2', 'ExSeq_13k', 'mdv2', 'Root', 'SAMPLE_13k', 40);
 insert into SAMPLES_DATASET (ID, EXP, VER, PARENT, TBL, SORT) values('samples_26k_mdv3', 'ExSeq_16k', 'mdv3', 'Root', 'SAMPLE_26k', 50);
 
@@ -49,6 +59,27 @@ delete from SAMPLES_PROP_ID;
 insert into SAMPLES_PROP_ID select pr.PROP, sa.ID from SAMPLES_DATASET sa, SAMPLES_PROP pr;
 
 
+-- ID_PH
+-- drop table if exists
+drop table if exists SAMPLES_ID_PH;
+
+-- create table
+create table SAMPLES_ID_PH select * from ID_PH;
+
+-- remove all old rows
+delete from SAMPLES_ID_PH;
+
+
+
+-- PROP_ID_PH
+-- drop table if exists
+drop table if exists SAMPLES_PROP_ID_PH;
+
+-- create table
+create table SAMPLES_PROP_ID_PH select * from PROP_ID_PH;
+
+-- remove all old rows
+delete from SAMPLES_PROP_ID_PH;
 
 
 
