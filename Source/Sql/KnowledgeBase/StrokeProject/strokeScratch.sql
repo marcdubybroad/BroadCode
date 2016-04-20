9214972bafda78ea413c172ccba42f21283077f8
mduby <mduby@broadinstitute.org>
2016-04-20 13:19:20 -0400

    added 17k to datasets

--- Source/Sql/KnowledgeBase/StrokeProject/strokeScratch.sql
+++ Source/Sql/KnowledgeBase/StrokeProject/strokeScratch.sql
@@ -46,3 +46,12 @@ update DATASET set SUBJECTS = 1201 where ID = 'GWAS_Stroke_gerfhs_mdv5';
 update DATASET set SUBJECTS = 5463 where ID = 'GWAS_Stroke_race_mdv5';
 update DATASET set SUBJECTS = 571 where ID = 'GWAS_Stroke_malmo_mdv5';
 update DATASET set SUBJECTS = 9067 where ID = 'GWAS_Stroke_mdv5';
+
+
+
+-- changes requested 20160420
+update DATASET set SUBJECTS = 773 where ID = 'GWAS_Stroke_gocha_mdv5';
+update DATASET set SUBJECTS = 967 where ID = 'GWAS_Stroke_europe_mdv5';
+update DATASET set SUBJECTS = 1336 where ID = 'GWAS_Stroke_gerfhs_mdv5';
+update DATASET set SUBJECTS = 3076 where ID = 'GWAS_Stroke_mdv5';
+
